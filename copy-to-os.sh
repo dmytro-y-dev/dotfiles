4451a5fc0675c3e15a6e28ef68afd74e3cbda64a
Dmytro Yurchenko <the.best.specialist@gmail.com>
2019-08-25 16:57:11 +0200

    Fix tmux config

--- copy-to-os.sh
+++ copy-to-os.sh
@@ -9,16 +9,19 @@ mkdir -p $TARGET_DIR/.config/tmuxinator
 mkdir -p $TARGET_DIR/.zsh/completions
 
 cp $CUR_DIR/zsh/.zshrc $TARGET_DIR/.zshrc
-cp $CUR_DIR/tmux/.tmux.conf.local $TARGET_DIR/.tmux.conf.local
 cp $CUR_DIR/alacritty/alacritty.yml $TARGET_DIR/.config/alacritty/alacritty.yml
-cp $CUR_DIR/nvim/init.vim $TARGET_DIR/.config/nvim/init.vim
-cp $CUR_DIR/mycli/.myclirc $TARGET_DIR/.myclirc
-cp $CUR_DIR/start-workspace.sh $TARGET_DIR/start-workspace.sh
-cp $CUR_DIR/tmuxinator/* $TARGET_DIR/.config/tmuxinator
 
+cp $CUR_DIR/tmux/.tmux.conf.local $TARGET_DIR/.tmux.conf.local
+cp $CUR_DIR/tmuxinator/* $TARGET_DIR/.config/tmuxinator
+ln -s $TARGET_DIR/.tmux.conf.local $TARGET_DIR/.tmux.conf
+
+cp $CUR_DIR/mycli/.myclirc $TARGET_DIR/.myclirc
+
+cp $CUR_DIR/start-workspace.sh $TARGET_DIR/start-workspace.sh
+
+cp $CUR_DIR/nvim/init.vim $TARGET_DIR/.config/nvim/init.vim
 if [ ! -d $TARGET_DIR/.config/nvim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $TARGET_DIR/.config/nvim/bundle/Vundle.vim
 fi
-
-vim +PluginInstall +qall
+eval vim +PluginInstall +qall
 
